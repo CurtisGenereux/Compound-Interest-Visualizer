9853819dd3bc0a3ec026d46deafe3a138f5462bb
Curtis Genereux <92477925+CurtisGenereux@users.noreply.github.com>
2023-04-26 15:56:49 -0600

    Update graphCreator.js

--- graphCreator.js
+++ graphCreator.js
@@ -1,12 +1,18 @@
 function createGraph() {
+    const startYear = 0;
+
+    const yearDataArray = balanceByYear.map((value, index) => {
+        const year = startYear + (index / 365);
+        return [year, value];
+    });
 
     Highcharts.chart('container', {
         chart: {
-                type: 'line'
-            },
+            type: 'line'
+        },
 
         title: {
-            text: "Growth of Investment Over " + yearsValue + " Years"
+            text: "Growth of Over " + yearsValue + " Years"
         },
 
         subtitle: {
@@ -20,15 +26,26 @@ function createGraph() {
         },
 
         xAxis: {
+            
+            type: 'linear',
+            title: {
+                text: 'Years'
+            },
+            labels: {
+                formatter: function () {
+                    return Math.round(this.value); 
+                }
+            },
             accessibility: {
                 rangeDescription: 'Years'
             }
         },
 
         tooltip: {
-        valueDecimals: 2,
-        valuePrefix: '$',
-
+            headerFormat: '<span style="color: #9e6a10;"><b>Investment:</b><br><span style="color: black;">',
+            pointFormat: 'Years: <b>{point.x:.2f}</b><br>Value: <b>${point.y:,.2f}</b>',
+            valueDecimals: 2,
+            valuePrefix: '$'
         },
 
         legend: {
@@ -46,30 +63,15 @@ function createGraph() {
                 label: {
                     connectorAllowed: false
                 },
-                pointStart: 1
+                pointStart: startYear,
+                pointIntervalUnit: 'year'
             }
         },
-        
 
         series: [{
             name: "Investment",
-            data: balanceByYear,
+            data: yearDataArray,
         }],
-
-        responsive: {
-            rules: [{
-                condition: {
-                    maxWidth: 500
-                },
-                chartOptions: {
-                    legend: {
-                        layout: 'horizontal',
-                        align: 'center',
-                        verticalAlign: 'bottom'
-                    }
-                }
-            }]
-        }
 
     });
 }
